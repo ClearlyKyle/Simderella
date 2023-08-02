fd7ed80389e6ee638d0fb2056bdb645837a62f88
Kyle McIntosh <kylemcintosh3@hotmail.co.uk>
2023-08-02 21:46:18 +0100

    updated utility functions

--- src/utils/utils.h
+++ src/utils/utils.h
@@ -6,44 +6,21 @@
 
 #define LOG_UNUSED(val) ((void)(val))
 
-#if 0
+#ifdef _DEBUG
+    #define ASSERT(expr) assert(expr)
 
-//#ifdef __GNUC__
-//#define LOG_LIKELY(expr)   __builtin_expect((expr), 1)
-//#define LOG_UNLIKELY(expr) __builtin_expect((expr), 0)
-//#else
-//#define LOG_LIKELY(expr)   (expre)
-//#define LOG_UNLIKELY(expr) (expre)
-//#endif
+    #define LOG(format, ...)  fprintf(stdout, "[DEBUG] " format, ##__VA_ARGS__)
+    #define LOGE(format, ...) fprintf(stderr, "[DEBUG] " format, ##__VA_ARGS__)
 
-#define DEBUG_MODE_PRINT printf("DEBUG MODE\n")
+    #define CHECK_ARRAY_BOUNDS(index, size) ASSERT(index >= 0 && index < size && "Array index out of bounds!")
 
-#define ASSERT(expr) assert(expr)
-
-//#define ASSERT(expression)         \
-//    if (LOG_LIKELY(!(expression))) \
-//    fprintf(stderr, "assertion failed : %s\n", #expression)
-
-#define LOG(format, ...)                fprintf(stdout, "[DEBUG] " format, ##__VA_ARGS__)
-#define LOGE(format, ...)               fprintf(stderr, "[ERROR] " format, ##__VA_ARGS__)
-
-assert
-/**
- * @brief check the index is in the bounds of the array
- *
- * @param index, which represents the index you want to access or modify in the array
- * @param size, which represents the max size of the array (max element)
- */
-#define CHECK_ARRAY_BOUNDS(index, size) assert(index >= 0 && index < size && "Array index out of bounds!")
-
+    #define DEBUG_MODE_PRINT LOG("DEBUG MODE\n")
 #else
-
-#define DEBUG_MODE_PRINT
-#define ASSERT(expression)
-#define LOG(format, ...)
-#define LOGE(format, ...)
-#define CHECK_ARRAY_BOUNDS(index, size)
-
+    #define DEBUG_MODE_PRINT
+    #define ASSERT(expression)
+    #define LOG(format, ...)
+    #define LOGE(format, ...)
+    #define CHECK_ARRAY_BOUNDS(index, size)
 #endif
 
 #endif // __UTILS_H__
