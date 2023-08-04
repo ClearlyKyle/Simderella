df91d6bf37c0665f67616bfe2ad33047e7658c50
Kyle McIntosh <kylemcintosh3@hotmail.co.uk>
2023-08-04 21:46:56 +0100

    remove path

--- src/raster/tex.c
+++ src/raster/tex.c
@@ -10,9 +10,6 @@ texture_t Texture_Load(const char *file_path, const int bbp)
     // textures oriented tha same as you view them in paint
     stbi_set_flip_vertically_on_load(1);
 
-    // char buff[64]; /* add file path to res folder */
-    // sprintf_s(buff, 64, "../../res/%s", file_path);
-
     unsigned char *data = stbi_load(file_path, &t.w, &t.h, &t.bpp, bbp);
     if (!data)
     {
